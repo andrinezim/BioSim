f7a312279816b8db7ca5e7f37a2ca2cb759ca81e
Andrine Zimmermann <andrine.zimmermann@nmbu.no>
2022-01-04 11:53:47 +0100

    Creating function templates for herbivores class.

--- src/biosim/animals.py
+++ src/biosim/animals.py
@@ -7,3 +7,21 @@ class Herbivores:
     def __init__(self, age, weight):
         self.age = age
         self.weight = weight
+
+    def feeding(self):
+        pass
+
+    def procreation(self):
+        pass
+
+    def migration(self):
+        pass
+
+    def aging(self):
+        pass
+
+    def weightloss(self):
+        pass
+
+    def death(self):
+        pass
