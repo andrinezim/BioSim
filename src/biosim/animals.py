5d61c344d14484ce07a73c1a8dd6e07a5ba9bc86
Andrine Zimmermann <andrine.zimmermann@nmbu.no>
2022-01-04 13:50:31 +0100

    Filled out aging and fitness function. Started on weightchange function.

--- src/biosim/animals.py
+++ src/biosim/animals.py
@@ -1,26 +1,37 @@
 """
 Module for herbivores
 """
+import math
 
 class Herbivores:
 
-    def __init__(self, age, weight):
+    def __init__(self, age, weight, f_max=800):
         self.age = age
         self.weight = weight
+        self.f_max = f_max
 
-    def feeding(self):
+
+    def aging(self):
+        self.age += 1
+
+    def fitness(self, a_half=40, phi_age=0.6, w_half=10, phi_weight=0.1):
+        if self.weight == 0:
+            phi = 0
+        else:
+            phi = (1/(1+math.exp(phi_age*(self.age-a_half)))) * (1/(1+math.exp(-phi_weight*(self.weight-w_half))))
+        return phi
+
+    def feeding(self, F):
+        pass
+
+    def weight_change(self, beta, eta):
+        self.weight -= eta*self.weight
         pass
 
     def procreation(self):
         pass
 
     def migration(self):
-        pass
-
-    def aging(self):
-        pass
-
-    def weightloss(self):
         pass
 
     def death(self):
