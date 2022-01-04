13456b1e645e9f86bf689a66d8f49f1fd098496c
Andrine Zimmermann <andrine.zimmermann@nmbu.no>
2022-01-04 15:38:03 +0100

    Creating script file. Writing pass on unnecessary fuctions for this part of the assignment.

---
+++ scripts/herbs_lowland_script.py
@@ -0,0 +1,7 @@
+"""
+Script for herbivores and lowland.
+"""
+
+from biosim.animals import Herbivores
+from biosim.landscapes import Lowland
+
--- src/biosim/simulation.py
+++ src/biosim/simulation.py
@@ -75,18 +75,23 @@ class BioSim:
 
         :param population: List of dictionaries specifying population
         """
+        pass
 
     @property
     def year(self):
         """Last year simulated."""
+        pass
 
     @property
     def num_animals(self):
         """Total number of animals on island."""
+        pass
 
     @property
     def num_animals_per_species(self):
         """Number of animals per species in island, as dictionary."""
+        pass
 
     def make_movie(self):
         """Create MPEG4 movie from visualization images saved."""
+        pass
