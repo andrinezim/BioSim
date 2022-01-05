e607a56ea796db2097038dcdbc22a795e25fcbc0
Andrine Zimmermann <andrine.zimmermann@nmbu.no>
2022-01-05 11:35:14 +0100

    Created dictinorary with default parameters. Did changes i fitness function because of this. Also created a template for function to set parameters.

--- src/biosim/animals.py
+++ src/biosim/animals.py
@@ -5,6 +5,34 @@ from .landscapes import Lowland
 from math import exp
 
 class Herbivores:
+    """
+    Class for herbivores.
+    """
+
+    default_params = {
+                    "w_birth": 8.0,
+                    "sigma_birth": 1.5,
+                    "beta": 0.9,
+                    "eta": 0.05,
+                    "a_half": 40.0,
+                    "phi_age": 0.6,
+                    "w_half": 10.0,
+                    "phi_weight": 0.1,
+                    "mu": 0.25,
+                    "gamma": 0.2,
+                    "zeta": 3.5,
+                    "xi": 1.2,
+                    "omega": 0.4,
+                    "F": 10.0,
+                    "DeltaPhiMax": None}
+
+    @classmethod
+    def set_params(cls, incoming_params):
+        """
+        Method for setting parameters.
+
+        :param incoming_params: Dictionary with parameters to replace default parameters.
+        """
 
     def __init__(self, age, weight):
         self.age = age
@@ -31,12 +59,12 @@ class Herbivores:
         q = (1 / (1 + exp(pos_neg * phi_aw * (x - x_half))))
         return q
 
-    def fitness(self, a_half=40, phi_age=0.6, w_half=10, phi_weight=0.1):
+    def fitness(self):
         if self.weight == 0:
             phi = 0
         else:
-            q_pos = self.q(self.age, a_half, phi_age, 1)
-            q_neg = self.q(self.weight, w_half, phi_weight, -1)
+            q_pos = self.q(self.age, default_params['a_half'], default_params['phi_age'], 1)
+            q_neg = self.q(self.weight, default_params['w_half'], default_params['phi_weight'], -1)
             phi = q_pos * q_neg
         return phi
 
