f8dd748f0b3be748b33875a2616737fbd98301a1
Andrine Zimmermann <andrine.zimmermann@nmbu.no>
2022-01-07 16:47:26 +0100

    Calculated the mean value of herbivores in one cell in the script2 file to try out the code. Ended on 210, but the number should be closer to 230

--- scripts/script2.py
+++ scripts/script2.py
@@ -1,4 +1,6 @@
 from biosim.landscapes import Lowland
+import matplotlib.pyplot as plt
+import statistics
 
 poph = [{'species': 'Herbivore',
             'age': 5,
@@ -8,12 +10,24 @@ lo = Lowland()
 params_fodder = {"f_max": 800}
 lo.set_params(params_fodder)
 lo.herbs_population(poph)
+testlist = []
+
 for i in range(300):
     print(lo.count_herbs())
     lo.eating_process()
     lo.animal_gives_birth()
     lo.animal_dies()
     lo.animal_gets_older()
+
+    amount = lo.count_herbs()
+    testlist.append(amount)
+
+print(testlist)
+print(statistics.mean(testlist))
+
+#plt.rcParams['figure.figsize'] = (12, 5)
+#plt.plot([i for i in range(300)], amount)
+#plt.legend()
 
 #antall = lo.count_herbs()
 #print(antall)
