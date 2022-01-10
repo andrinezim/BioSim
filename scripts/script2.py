f5454a2332a468c2e950e68c75ed3545b186e1d0
Andrine Zimmermann <andrine.zimmermann@nmbu.no>
2022-01-10 17:24:30 +0100

    Changed script2, so the plot shows the correct values compared to what was shown in the lecture.

--- scripts/script2.py
+++ scripts/script2.py
@@ -2,7 +2,6 @@ from biosim.landscapes import Lowland
 from biosim.island import Island
 
 import matplotlib.pyplot as plt
-import statistics
 import random
 import textwrap
 
@@ -29,7 +28,7 @@ ini_carns = [{'loc': (1, 1),
                       for _ in range(20)]}]
 
 # lo = Lowland()
-isl = Island("L", ini_herbs)
+#isl = Island("L", ini_herbs)
 
 params_fodder = {"f_max": 800}
 
@@ -37,32 +36,44 @@ Lowland.set_params(params_fodder)
 # lo.animals_population(poph)
 # isl.adding_population(ini_carns)
 
-testlist = []
+plt.figure(figsize=(8, 6))
 
-for i in range(50):
-    lo = isl.map[(1, 1)]
+for seed in range(100, 120):
+    random.seed(seed)
+
+    herb_list = []
+    carn_list = []
+    isl = Island("L", ini_herbs)
+
+    for i in range(50):
+        lo = isl.map[(1, 1)]
+        # print(lo.amount_herbs, lo.amount_carns)
+        lo.eating_process()
+        lo.animal_gives_birth()
+        lo.animal_gets_older()
+        lo.animal_dies()
+
+        herb_list.append(lo.amount_herbs)
+        carn_list.append(lo.amount_carns)
+
+    isl.adding_population(ini_carns)
+    for i in range(50, 300):
+        lo = isl.map[(1, 1)]
+        # print(lo.amount_herbs, lo.amount_carns)
+        lo.eating_process()
+        lo.animal_gives_birth()
+        lo.animal_gets_older()
+        lo.animal_dies()
+
+        herb_list.append(lo.amount_herbs)
+        carn_list.append(lo.amount_carns)
+
     print(lo.amount_herbs, lo.amount_carns)
-    lo.eating_process()
-    lo.animal_gives_birth()
-    lo.animal_dies()
-    lo.animal_gets_older()
 
-    testlist.append(lo.amount_herbs)
+    plt.plot(herb_list, 'b')
+    plt.plot(carn_list, 'r')
 
-isl.adding_population(ini_carns)
-for i in range(250):
-    lo = isl.map[(1, 1)]
-    print(lo.amount_herbs, lo.amount_carns)
-    lo.eating_process()
-    lo.animal_gives_birth()
-    lo.animal_dies()
-    lo.animal_gets_older()
-
-    testlist.append(lo.amount_herbs)
-
-
-
-print(testlist)
+plt.show()
 
 # print(statistics.mean(testlist))
 
