8f92b5e3d3e3b94b13db79aeb0d6d95f15a6f9d1
Karin Mollatt <karin.mollatt@nmbu.no>
2022-01-11 12:01:34 +0100

    Created template for test methods in test_animals.

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -2,3 +2,26 @@
 __author__ = 'Andrine Zimmermann, Karin Mollatt'
 __email__ = 'andrine.zimmermann@nmbu.no, karin.mollatt@nmbu.no'
 
+"""
+Testing the functions in animals.py
+"""
+
+import pytest
+
+def test_set_params():
+    pass
+
+def test_negative_param_value():
+    pass
+
+def test_deltaphimax_negative():
+    pass
+
+def test_eta_0_1():
+    pass
+
+def test_param_update():
+    pass
+
+def test_invalid_param():
+    pass
