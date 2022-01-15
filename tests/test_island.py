d65b6e7fba7c82493361a9756bf84dca18fdcda4
Andrine Zimmermann <andrine.zimmermann@nmbu.no>
2022-01-15 14:36:30 +0100

    Created template for the test_island file.

--- tests/test_island.py
+++ tests/test_island.py
@@ -1,3 +1,37 @@
 
 __author__ = 'Andrine Zimmermann, Karin Mollatt'
 __email__ = 'andrine.zimmermann@nmbu.no, karin.mollatt@nmbu.no'
+
+import pytest
+from biosim.island import Island
+
+
+class TestIsland:
+
+    # Test for creating_map
+    def test_creating_map(self):
+        pass
+
+    # Test for adding_population
+    def test_key_error_invalid_location(self):
+        pass
+
+    # Test for animals_per_species
+    def test_animals_per_species(self):
+        pass
+
+    # Test for find_adjacent_cell_migrate
+    def find_adjacent_cell(self):
+        # mocker for random choice???
+        pass
+
+    # Test for migrating_animals
+    def available_landscape_migrate(self):
+        pass
+
+    # Test for restart_migration
+    def test_restart_migration_to_false(self):
+        pass
+
+
+pytest.main(['test_island.py'])
