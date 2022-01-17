41e5d91ceee516ede9efc8c1246e97ab76577f80
Andrine Zimmermann <andrine.zimmermann@nmbu.no>
2022-01-17 18:13:28 +0100

    Further tests in test_island.

--- tests/test_island.py
+++ tests/test_island.py
@@ -4,6 +4,7 @@ __email__ = 'andrine.zimmermann@nmbu.no, karin.mollatt@nmbu.no'
 
 import pytest
 from biosim.island import Island
+from biosim.landscapes import Lowland
 
 
 class TestIsland:
@@ -23,8 +24,22 @@ class TestIsland:
         self.standard_island = Island(ini_pop=ini_herbs, island_map="WWWWW\nWWLWW\nWLLLW\nWWLWW\nWWWWW")
 
     # Test for creating_map
-    def test_creating_map(self):
+    def test_invalid_landscape(self):
+        """
+        Method for raising an error if the landscape is invalid.
+        """
+        """with pytest.raises(ValueError):
+            self.standard_island.creating_map(island_map="WWW\nWRW\nWWW")"""
         pass
+
+    def test_creating_map(self):
+        """
+        Method for testing if a map is created.
+        """
+        """island_map_string = "WWWWW\nWWLWW\nWLLLW\nWWLWW\nWWWWW"
+        self.standard_island.creating_map(island_map_string)
+        key_list = self.standard_island.map.keys()
+        assert self.standard_island.map[(3, 3)] == type()"""
 
     # Test for adding_population
     def test_key_error_invalid_location(self):
@@ -39,10 +54,6 @@ class TestIsland:
         with pytest.raises(KeyError):
             Island(ini_pop=invalid_loc_pop, island_map="WWWWW\nWWLWW\nWLLLW\nWWLWW\nWWWWW")
 
-    # Test for animals_per_species
-    def test_animals_per_species(self):
-        pass
-
     # Test for find_adjacent_cell_migrate
     def find_adjacent_cell(self):
         # mocker for random choice???
@@ -50,10 +61,10 @@ class TestIsland:
 
     # Test for migrating_animals
     def available_landscape_migrate(self):
-        pass
-
-    # Test for restart_migration
-    def test_restart_migration_to_false(self):
+        """
+        Testing that if the landscape is available, it is possib
+        :return:
+        """
         pass
 
 
