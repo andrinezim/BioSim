5a12603d2ceca5f76cd459d3896758cec803d69d
Andrine Zimmermann <andrine.zimmermann@nmbu.no>
2022-01-18 11:30:36 +0100

    Created template for test_simulation file.

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -2,8 +2,33 @@
 __author__ = 'Andrine Zimmermann, Karin Mollatt'
 __email__ = 'andrine.zimmermann@nmbu.no, karin.mollatt@nmbu.no'
 
+import pytest
 
 from biosim.simulation import BioSim
 
 
+class TestSimulation:
 
+    @pytest.fixture
+    def standard_simulation(self):
+        pass
+
+    # Test to simulate function
+    def test_value_error_vis_years(self):
+        """
+        Testing that if
+        :return:
+        """
+        pass
+
+    def test_current_year_increase(self):
+        pass
+
+    def test_get_year(self):
+        pass
+
+    def test_get_animal_per_species(self):
+        pass
+
+
+pytest.main('simulation.py')
